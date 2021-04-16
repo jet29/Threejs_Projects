acbf982e3f8378450ba227106c2ca88862cc880a
Jose Tirado <jose_enrique_29@hotmail.com>
2021-04-16 14:45:23 -0400

    feat: [basic_template_jt] adding threejs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-import _ from 'lodash';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'arreloco'], ' ');
-    return element;
+let controls, scene, camera, renderer;
+
+function init(){
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    
+    controls = new OrbitControls( camera, renderer.domElement );
 }
 
-document.body.appendChild(component());
+function addModels(){
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+    
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
+
+	renderer.render( scene, camera );
+}
+
+init();
+addModels();
+animate();
