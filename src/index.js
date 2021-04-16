6f5ae752fd56f3bca385a44ad0bb95711c0096ba
Jose Tirado <jose_enrique_29@hotmail.com>
2021-04-16 14:50:34 -0400

    feat: [basic_template_jt] adding sky but not working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Sky } from 'three/examples/jsm/objects/Sky.js';
 
-let controls, scene, camera, renderer;
+let controls, scene, camera, renderer, sky;
 
 function init(){
     scene = new THREE.Scene();
+    window.scene = scene;
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-    
+
     controls = new OrbitControls( camera, renderer.domElement );
+}
+
+function addSky(){
+    sky = new Sky();
+    sky.scale.setScalar( 450000 );
+    scene.add( sky );
 }
 
 function addModels(){
@@ -33,5 +41,6 @@ function animate() {
 }
 
 init();
+addSky();
 addModels();
 animate();
